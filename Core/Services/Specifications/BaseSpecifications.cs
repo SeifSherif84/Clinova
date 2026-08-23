1740e4b73c87ddfb738f4da0e6580bd553f91e3f
Seif Sherif <seif27168@gmail.com>
2026-08-24 02:51:52 +0300

    Add Data Access Pattern

---
+++ Core/Services/Specifications/BaseSpecifications.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specifications
+{
+    public class BaseSpecifications<TEntity, TKey> : IBaseSpecifications<TEntity, TKey> where TEntity : class
+    {
+        public List<Expression<Func<TEntity, object>>> Includes { get; set; }
+        public Expression<Func<TEntity, bool>>? Criteria { get; set; }
+        public Expression<Func<TEntity, object>>? OrderBy { get; set; }
+        public Expression<Func<TEntity, object>>? OrderByDescending { get; set; }
+        public bool IsPaginationEnabled { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+
+        public BaseSpecifications()
+        {
+            Includes = new List<Expression<Func<TEntity, object>>>();
+            Criteria = null;
+            OrderBy = null;
+            OrderByDescending = null;
+            IsPaginationEnabled = false;
+            Skip = 0;
+            Take = 0;
+        }
+    }
+}
---
+++ Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts;
+using Persistence.Data.Contexts;
+using Persistence.Repositories;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.UnitOfWork
+{
+    public class UnitOfWork(AppDbContext _context) : IUnitOfWork
+    {
+        private ConcurrentDictionary<string, object> _repositories = new ConcurrentDictionary<string, object>();
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : class
+        {
+            return (IGenericRepository<TEntity, TKey>) _repositories.GetOrAdd(typeof(TEntity).Name, new GenericRepositiry<TEntity, TKey>(_context));
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
